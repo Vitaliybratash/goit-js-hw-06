96a8b531f693ed207c9eea04ee5fa6734d43e665
Vitaliy <vitaliy.bratash23@gmail.com>
2022-08-26 22:32:13 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -4,16 +4,17 @@ const button = inputs.pop();
 
 const submitHandler = (event) => {
   event.preventDefault();
-  const isValid = inputs.filter(input => input.value !== '').length === inputs.length
-  if(isValid) {
-    const user = {} 
-    user.email = inputs[0].value
-    user.password = inputs[1].value
-    console.log(user)
+  const isValid =
+    inputs.filter((input) => input.value !== "").length === inputs.length;
+  if (isValid) {
+    const user = {};
+    user.email = inputs[0].value;
+    user.password = inputs[1].value;
+    console.log(user);
+    form.reset();
   } else {
-    alert( 'всі поля повинні бути заповнені.')
+    alert("всі поля повинні бути заповнені.");
   }
-
 };
 
-form.addEventListener("submit" , submitHandler);
+form.addEventListener("submit", submitHandler);
